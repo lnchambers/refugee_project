14abfb758751bc083c8620378472eacab578f003
Luke Chambers <lukenatchambers@gmail.com>
2018-04-03 20:19:46 -0600

    Get tests passing

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
 
   root "welcome#index"
 
-  get "/about", :to => "about#index"
-
   get "/login", to: "sessions#new"
 
   post "/login", to: "sessions#create"
 
   delete "/logout", to: "sessions#destroy"
+
+  get "/dashboard", to: 'dashboard#show'
+
+  resources :requests, only: [:show, :new, :create]
+
+  namespace :admin do
+    resources :users, only: [:new, :create, :update, :delete]
+    get "/dashboard", to: 'users#show'
+  end
 
 end
