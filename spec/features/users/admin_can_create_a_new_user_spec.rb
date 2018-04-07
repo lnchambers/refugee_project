28e5a5712588e4111e34f878d7a4a6b270fd2902
Luke Chambers <lukenatchambers@gmail.com>
2018-04-06 20:03:32 -0600

    Tighten up testing

---
+++ spec/features/users/admin_can_create_a_new_user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "As an admin" do
+  before :each do
+    @admin = create(:admin)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+  end
+  describe "when I visit my dashboard page" do
+    it "I can see a link to create a new user" do
+      visit admin_dashboard_path
+
+      click_on "Create new User"
+
+      expect(current_path).to eq("/admin/users/new")
+    end
+  end
+end
