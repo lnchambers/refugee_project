93dabe2ef9d280b060a39868bfe3bac3cc5721f0
Luke Chambers <lukenatchambers@gmail.com>
2018-04-09 14:36:20 -0600

    Remove redundant code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,9 +4,6 @@ class DashboardController < ApplicationController
   def show
     @user = User.find(current_user.id)
     @requests = Request.where(user_id: current_user.id)
-    unless @user == current_user
-      raise ActionController::RoutingError.new('Not Found')
-    end
   end
 
 end
