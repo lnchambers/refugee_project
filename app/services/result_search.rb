29bf760246e641cc653d144597527df8a5fd8e61
Luke Chambers <lukenatchambers@gmail.com>
2018-04-12 19:37:45 -0600

    Set up requests for production

--- app/services/result_search.rb
+++ app/services/result_search.rb
@@ -14,7 +14,7 @@ class ResultSearch
     attr_reader :request, :status
 
     def conn
-      Faraday.get("http://localhost:3000/api/v1/results?age=#{request.age}&name=#{request.name}&gender=#{request.gender}&country_of_origin=#{request.country_of_origin}&group_size=#{request.group_size}&country_of_seperation=#{request.country_of_separation}&client_secret=#{ENV['CLIENT_SECRET']}&client_id=#{ENV['CLIENT_ID']}")
+      Faraday.get("https://dry-mountain-22730.herokuapp.com/api/v1/results?age=#{request.age}&name=#{request.name}&gender=#{request.gender}&country_of_origin=#{request.country_of_origin}&group_size=#{request.group_size}&country_of_seperation=#{request.country_of_separation}&client_secret=#{ENV['CLIENT_SECRET']}&client_id=#{ENV['CLIENT_ID']}")
     end
 
     def parsed_response
